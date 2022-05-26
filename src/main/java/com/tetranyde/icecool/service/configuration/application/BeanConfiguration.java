ebd3534d263f06846e2e9b2b43f764a0a0a766db
achiradesilva123 <achiradesilva123@gmail.com>
2022-05-26 19:51:18 +0500

    initial commit

--- src/main/java/com/tetranyde/icecool/service/configuration/application/BeanConfiguration.java
+++ src/main/java/com/tetranyde/icecool/service/configuration/application/BeanConfiguration.java
@@ -1,18 +1,9 @@
 package com.tetranyde.icecool.service.configuration.application;
 
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
-
-import org.springframework.web.client.RestTemplate;
 
 @Configuration
 public class BeanConfiguration {
-
-    @Bean
-    public RestTemplate getRestTemplate() {
-        return new RestTemplate(new HttpComponentsClientHttpRequestFactory());
-    }
 
 
 }
